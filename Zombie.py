620e243a39ecd79a68ce3ce572f694926b110a4e
Jcloidt78 <100542465+Jcloidt78@users.noreply.github.com>
2023-10-03 08:21:50 -0400

    Zombie.py

--- Zombie.py
+++ Zombie.py
@@ -34,35 +34,50 @@ class Zombie(Agent):
     def __init__(self, agent_id, energy_range):
         super().__init__(agent_id, energy_range)
 
-    def bite(self, human):
-        if human.state != "INFECTED":
-            human.change_state("INFECTED")
+    def bite_human(self):
+        if self.neighbors:
+            human = random.choice(self.neighbors)
             human.reduce_energy(10)
-            self.increase_energy(10)
+            if human.state != "INFECTED":
+                human.change_state("INFECTED")
+                self.increase_energy(10)
 
 class Doctor(Human):
     def __init__(self, agent_id, energy_range):
         super().__init__(agent_id, energy_range)
 
-    def cure(self, human):
-        if human.state == "INFECTED":
-            human.change_state("ALIVE")
+    def cure_infected_human(self):
+        if self.neighbors:
+            infected_human = random.choice(self.neighbors)
+            if infected_human.state == "INFECTED":
+                infected_human.change_state("ALIVE")
+
+# Simulation parameters
+num_agents = 100
+energy_range = (0, 100)
+num_iterations = 100
 
 # Create agents
-humans = [Human(i, [0, 100]) for i in range(10)]
-zombies = [Zombie(i, [0, 100]) for i in range(5)]
-doctors = [Doctor(i, [0, 100]) for i in range(3)]
+agents = []
+for i in range(num_agents):
+    if i % 3 == 0:
+        agent = Zombie(i, energy_range)
+    elif i % 3 == 1:
+        agent = Doctor(i, energy_range)
+    else:
+        agent = Human(i, energy_range)
+    agents.append(agent)
 
-# Add neighbors randomly
-for human in humans:
-    human.add_neighbor(random.choice(humans + zombies + doctors))
-for zombie in zombies:
-    zombie.add_neighbor(random.choice(humans + zombies + doctors))
-for doctor in doctors:
-    doctor.add_neighbor(random.choice(humans + zombies + doctors))
+# Randomly assign neighbors
+for agent in agents:
+    num_neighbors = random.randint(0, num_agents)
+    for _ in range(num_neighbors):
+        neighbor = random.choice(agents)
+        if neighbor != agent:
+            agent.add_neighbor(neighbor)
 
-# Simulate the game over time
-time_series = []
+# Simulation loop
+statuses = []
 alive_humans = []
 infected_humans = []
 dead_humans = []
@@ -72,49 +87,50 @@ dead_doctors = []
 alive_zombies = []
 dead_zombies = []
 
-for t in range(100):
+for _ in range(num_iterations):
     # Update agent statuses
-    time_series.append([agent.__dict__ for agent in humans + zombies + doctors])
+    status = {}
+    for agent in agents:
+        status[agent.agent_id] = {
+            "Energy": agent.energy,
+            "State": agent.state,
+            "Neighbors": [neighbor.agent_id for neighbor in agent.neighbors]
+        }
+    statuses.append(status)
 
     # Count populations
-    alive_humans.append(len([human for human in humans if human.state == "ALIVE"]))
-    infected_humans.append(len([human for human in humans if human.state == "INFECTED"]))
-    dead_humans.append(len([human for human in humans if human.state == "DEAD"]))
-    alive_doctors.append(len([doctor for doctor in doctors if doctor.state == "ALIVE"]))
-    infected_doctors.append(len([doctor for doctor in doctors if doctor.state == "INFECTED"]))
-    dead_doctors.append(len([doctor for doctor in doctors if doctor.state == "DEAD"]))
-    alive_zombies.append(len([zombie for zombie in zombies if zombie.state == "ALIVE"]))
-    dead_zombies.append(len([zombie for zombie in zombies if zombie.state == "DEAD"]))
+    alive_humans.append(len([agent for agent in agents if isinstance(agent, Human) and agent.state == "ALIVE"]))
+    infected_humans.append(len([agent for agent in agents if isinstance(agent, Human) and agent.state == "INFECTED"]))
+    dead_humans.append(len([agent for agent in agents if isinstance(agent, Human) and agent.state == "DEAD"]))
+    alive_doctors.append(len([agent for agent in agents if isinstance(agent, Doctor) and agent.state == "ALIVE"]))
+    infected_doctors.append(len([agent for agent in agents if isinstance(agent, Doctor) and agent.state == "INFECTED"]))
+    dead_doctors.append(len([agent for agent in agents if isinstance(agent, Doctor) and agent.state == "DEAD"]))
+    alive_zombies.append(len([agent for agent in agents if isinstance(agent, Zombie) and agent.state == "ALIVE"]))
+    dead_zombies.append(len([agent for agent in agents if isinstance(agent, Zombie) and agent.state == "DEAD"]))
 
-    # Perform actions
-    for human in humans:
-        if random.random() < 0.5:
-            human.reduce_energy(5)
-    for zombie in zombies:
-        if random.random() < 0.5:
-            zombie.reduce_energy(5)
-            if zombie.neighbors:
-                zombie.bite(random.choice(zombie.neighbors))
-    for doctor in doctors:
-        if random.random() < 0.5:
-            doctor.increase_energy(5)
-            if doctor.neighbors:
-                doctor.cure(random.choice(doctor.neighbors))
+    # Simulate agent interactions
+    for agent in agents:
+        if isinstance(agent, Zombie):
+            agent.bite_human()
+        elif isinstance(agent, Doctor):
+            agent.cure_infected_human()
 
-# Save agent statuses to file
-df = pd.DataFrame(time_series)
-df.to_csv("agent_statuses.csv", index=False)
+# Output agent statuses to file
+df_statuses = pd.DataFrame(statuses)
+df_statuses.to_csv("agent_statuses.csv", index=False)
 
 # Plot population dynamics
-plt.plot(alive_humans, label="Alive Humans")
-plt.plot(infected_humans, label="Infected Humans")
-plt.plot(dead_humans, label="Dead Humans")
-plt.plot(alive_doctors, label="Alive Doctors")
-plt.plot(infected_doctors, label="Infected Doctors")
-plt.plot(dead_doctors, label="Dead Doctors")
-plt.plot(alive_zombies, label="Alive Zombies")
-plt.plot(dead_zombies, label="Dead Zombies")
-plt.xlabel("Time")
+time_steps = range(num_iterations)
+plt.plot(time_steps, alive_humans, label="Alive Humans")
+plt.plot(time_steps, infected_humans, label="Infected Humans")
+plt.plot(time_steps, dead_humans, label="Dead Humans")
+plt.plot(time_steps, alive_doctors, label="Alive Doctors")
+plt.plot(time_steps, infected_doctors, label="Infected Doctors")
+plt.plot(time_steps, dead_doctors, label="Dead Doctors")
+plt.plot(time_steps, alive_zombies, label="Alive Zombies")
+plt.plot(time_steps, dead_zombies, label="Dead Zombies")
+plt.xlabel("Time Steps")
 plt.ylabel("Population")
 plt.legend()
 plt.savefig("population_dynamics.png")
+plt.show()
